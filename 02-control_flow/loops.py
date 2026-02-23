2a4d34fdb6f2f4b1f5843b53c140d70a18ab6783
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2026-02-23 17:59:22 -0500

    Update loops.py

--- 02-control_flow/loops.py
+++ 02-control_flow/loops.py
@@ -1,10 +1,39 @@
 # Loops
-# a control flow statement that allows a block of code to be executed repeatedly
+# used to repeat code
 
-# While loops
+# while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1 # IMPORTANT: update variable to avoid infinite loop
 
-# For loops
+# for loop (with range())
+for i in range(5):
+    print(i) # range(stop)
+for i in range(1, 6):
+    print(i) # range(start, stop)
+for i in range(0, 10, 2):
+    print(i) #range*(start, stop, step)
 
-# Nested loops
+# looping through list
+colors = ["purple", "blue", "green"]
+for color in colors:
+    print(color)
 
-# 
+# break and continue
+for i in range(10):
+    if i == 5:
+        break # stop loop completely
+    print(i)
+for i in range(5):
+    if i == 2:
+        continue # skip this specific iteration
+    print(i)
+
+# nested loops
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
+# WARNING
+# if you forget to update the loop variable, you may create an infinite loop.
