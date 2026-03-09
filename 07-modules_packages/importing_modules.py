8e6ac796c01df6152366c5563f7b25a34410ca92
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2026-03-09 17:29:00 -0400

    Update importing_modules.py

---
+++ 07-modules_packages/importing_modules.py
@@ -0,0 +1,29 @@
+# Importing Modules
+# a modules is a python file containing code (functions, variables, etc)
+
+# Importing a module
+import math
+print(math.sqrt(81))
+print(math.pi)
+
+# Importing specific functions
+from math import sqrt, pi
+print(sqrt(16))
+print(pi)
+
+# Import with alias
+import math as m
+print(m.sqrt(25))
+
+# Import everything
+from math import *
+print(sqrt(9))
+
+# Common modules
+import random
+print(random.randint(1, 10))
+
+import datetime
+print(datetime.date.today())
+
+# Python searches for modules in the current folder, installed packages, and puthons standard library.
