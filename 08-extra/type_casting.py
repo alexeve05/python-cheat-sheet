2c04b0659f3a1c2128f6a3b4502dcbb2bce02555
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2026-03-09 17:52:45 -0400

    Update type_casting.py

---
+++ 08-extra/type_casting.py
@@ -0,0 +1,29 @@
+# Type Casting
+# converting one data type to another
+
+# String to number
+numStr = "16"
+num = int(numStr)
+print(num + 2)
+
+# Float conversion
+value = "3.14"
+pi = float(value)
+print(pi)
+
+# Number to string
+age = 21
+text = str(age)
+print("Age: " + text)
+
+# Boolean conversion
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("hi"))
+
+# Common input
+age = int(input("Enter your age: "))
+print(age + 1)
+
+# input() will always return a string.
